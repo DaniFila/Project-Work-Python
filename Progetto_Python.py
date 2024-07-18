c6a96e778bf1f9229269a2ff2fc1bce2f0a2d263
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-18 17:19:31 +0200

    Add files via upload

--- Progetto_Python.py
+++ Progetto_Python.py
@@ -3,79 +3,110 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 class DataSet: # classe DataSet
+
     def __init__(self,nome_file): # classe costruttore dove gli passo il percorso del file
-        self.df = pd.read_csv(nome_file) # col percorso legge il file csv e lo salva in un DataFrame
-        self.nome_file = nome_file
+        try:
+            self.df = pd.read_csv(nome_file) # col percorso legge il file csv e lo salva in un DataFrame
+            self.nome_file = nome_file
+            print(f"\nFile: {nome_file} Caricato con successo!")
+        except FileNotFoundError:
+            print(f"\nFile: {nome_file} non trovato!")
+        except:
+            print("\nErrore, non è stato possibile caricare il File!")
     
     def save_csv(self,nome_file): # metodo che salva il DataFrame in csv
-        self.df.to_csv(nome_file,index=False)
-        print("Salvato con successo!")
+        try:
+            self.df.to_csv(nome_file,index=False)
+            print(f"\nFile: {nome_file} Salvato con successo!")
+        except:
+            print(f"\nErrore: il file {nome_file} non è stato salvato!")
     
     def info(self): # metodo che stampa l'esplorazione dei dati, le info generali del dataframe, le statistiche dei dati, e il count del numero dei clienti in compagnia e non
-        print("\nDescrizione generale del Dataset:\n")
-        print(f"Informazioni:\n{self.df.info()}\n")
-        print(f"Statistiche:\n{self.df.describe()}\n")
-        print(f"Stato clienti nella compagnia:\n{self.df['Churn'].value_counts()}\n")
+        try:
+            print("\nDescrizione generale del Dataset:\n")
+            print(f"Informazioni:\n{self.df.info()}\n")
+            print(f"Statistiche:\n{self.df.describe()}\n")
+            print(f"Stato clienti nella compagnia:\n{self.df['Churn'].value_counts()}\n")
+        except:
+            print("\nErrore, non è stato possibile eseguire l'operazione!")
 
     def pulizia_dati(self): # metodo che pulisce i dati del DataFrame
         # Sostituzione valori mancanti nelle colonne Tariffa Mensile e Dati Consumate
-        self.df['Tariffa_Mensile'].fillna(self.df['Tariffa_Mensile'].median(), inplace=True)
-        self.df['Dati_Consumati'].fillna(self.df['Dati_Consumati'].mean(), inplace=True)
+        try:
+            self.df['Tariffa_Mensile'].fillna(self.df['Tariffa_Mensile'].median(), inplace=True)
+            self.df['Dati_Consumati'].fillna(self.df['Dati_Consumati'].mean(), inplace=True)
 
-        # Eliminazione delle righe con valori mancanti rimasti
-        self.df.dropna(inplace=True)
-        print("Eliminate righe con Valori mancanti rimasti\n")
+            # Eliminazione delle righe con valori mancanti rimasti
+            self.df.dropna(inplace=True)
+            print("Eliminate righe con Valori mancanti rimasti\n")
 
-        # Correzione di anomalie
-        self.df = self.df[self.df['Età'] > 0]
-        self.df = self.df[self.df['Tariffa_Mensile'] > 0]
-        self.df = self.df[self.df['Dati_Consumati'] > 0]
+            # Correzione di anomalie
+            self.df = self.df[self.df['Età'] > 0]
+            self.df = self.df[self.df['Tariffa_Mensile'] > 0]
+            self.df = self.df[self.df['Dati_Consumati'] >= 0]
 
-        print("DataSet Pulito con successo!\n")
-        self.save_csv(self.nome_file)
+            print("DataSet Pulito con successo!\n")
+            self.save_csv(self.nome_file)
+        except:
+            print("\nErrore, non è stato possibile eseguire la pulizia dei dati!")
 
     def aggiungi_colonna_costo_per_GB(self): # metodo che aggiunge una colonna al DataFrame
-        self.df['Costo_per_GB'] = self.df['Tariffa_Mensile'] / self.df['Dati_Consumati']
-        self.save_csv(self.nome_file)
-        print("Colonna aggiunta con successo!")
+        try:
+            self.df['Costo_per_GB'] = self.df['Tariffa_Mensile'] / self.df['Dati_Consumati']
+            self.save_csv(self.nome_file)
+            print("Colonna aggiunta con successo!")
+        except:
+            print("\nErrore, non è stato possibile aggiungere la colonna!")
 
     def esplorazione_dati(self): # metodo che esplora i dati del DataFrame, facendo una relazione tra i clienti che sono in compagnia e quelli no, con la media dell'età,durata abbonamento e tariffa mensile, ed effettua una correlazione tra tutti i dati
-        relazione = self.df.groupby("Churn").mean()[['Età', 'Durata_Abonnamento', 'Tariffa_Mensile']]
-        self.df['Churn'] = self.df['Churn'].map({'No': 0, 'Sì': 1}) # sostituisco momentaneamente le stringhe NO e SI con 0 e 1 per verificare le possibili correlazioni
-        correlazioni = self.df.corr()
-        self.df['Churn'] = self.df['Churn'].map({0: "No", 1: "Sì"})
-        print(f"\nTabella di relazione tra Età, Durata_Abonnamento, Tariffa_Mensile e la Churn:\n{relazione}\n")
-        print(f"\nCorrelazioni tra variabili:\n{correlazioni}\n")
+        try:
+            relazione = self.df.groupby("Churn").mean()[['Età', 'Durata_Abonnamento', 'Tariffa_Mensile']]
+            self.df['Churn'] = self.df['Churn'].map({'No': 0, 'Sì': 1}) # sostituisco momentaneamente le stringhe NO e SI con 0 e 1 per verificare le possibili correlazioni
+            correlazioni = self.df.corr()
+            self.df['Churn'] = self.df['Churn'].map({0: "No", 1: "Sì"})
+            print(f"\nTabella di relazione tra Età, Durata_Abonnamento, Tariffa_Mensile e la Churn:\n{relazione}\n")
+            print(f"\nCorrelazioni tra variabili:\n{correlazioni}\n")
+        except:
+            print("\nErrore, non è stato possibile effettuare l'operazione!")
 
     def preparazione_dati(self): # metodo che prepara i dati per la modellazione
-        self.df['Churn'] = self.df['Churn'].map({'No': 0, 'Sì': 1}) # imposto i valori nella colonna Churn con 0 e 1 al posto di Si e NO
-        try: # effettuo questa verifica nel caso in cui non fosse stata creata la colonna Costo_per_GB
-            colonne = ["ID_Cliente","Età","Durata_Abonnamento","Tariffa_Mensile","Dati_Consumati","Servizio_Clienti_Contatti","Churn","Costo_per_GB"]
-            for colonna in colonne:
-                self.df[colonna] = (self.df[colonna]- np.mean(self.df[colonna])) / np.std(self.df[colonna]) # normalizzo le colonne scandendo al lista contenenti i nomi delle colonne e andando a usare la formula del valore - la media del valore tutto diviso la deviazione standard
-            print("Colonne normalizzate con successo!")
-            self.save_csv("Corso Python/Giovedì 18/Progetto_Python_1_ready_modelling.csv") # salvo in un nuovo file i dataframe in formato csv pronto per la modellazione
+        try:
+            self.df['Churn'] = self.df['Churn'].map({'No': 0, 'Sì': 1}) # imposto i valori nella colonna Churn con 0 e 1 al posto di Si e NO
+            try: # effettuo questa verifica nel caso in cui non fosse stata creata la colonna Costo_per_GB
+                colonne = ["ID_Cliente","Età","Durata_Abonnamento","Tariffa_Mensile","Dati_Consumati","Servizio_Clienti_Contatti","Churn","Costo_per_GB"]
+                for colonna in colonne:
+                    self.df[colonna] = (self.df[colonna]- np.mean(self.df[colonna])) / np.std(self.df[colonna]) # normalizzo le colonne scandendo al lista contenenti i nomi delle colonne e andando a usare la formula del valore - la media del valore tutto diviso la deviazione standard
+                print("Colonne normalizzate con successo!")
+                self.save_csv("Corso Python/Giovedì 18/Progetto_Python_1_ready_modelling.csv") # salvo in un nuovo file i dataframe in formato csv pronto per la modellazione
+            except:
+                colonne = ["ID_Cliente","Età","Durata_Abonnamento","Tariffa_Mensile","Dati_Consumati","Servizio_Clienti_Contatti","Churn"]
+                for colonna in colonne:
+                    self.df[colonna] = (self.df[colonna]- np.mean(self.df[colonna])) / np.std(self.df[colonna]) 
+                print("Colonne normalizzate con successo!")
+                self.save_csv("Corso Python/Giovedì 18/Progetto_Python_1_ready_modelling.csv") 
         except:
-            colonne = ["ID_Cliente","Età","Durata_Abonnamento","Tariffa_Mensile","Dati_Consumati","Servizio_Clienti_Contatti","Churn"]
-            for colonna in colonne:
-                self.df[colonna] = (self.df[colonna]- np.mean(self.df[colonna])) / np.std(self.df[colonna]) 
-            print("Colonne normalizzate con successo!")
-            self.save_csv("Corso Python/Giovedì 18/Progetto_Python_1_ready_modelling.csv") 
+            print("\nErrore, non è stato possibile normalizzare le colonne!")
     
     def visualizza_statistiche_età(self):
-        plt.figure(figsize=(10,6)) # inizializzo le dimensioni della figura
-        sns.histplot(self.df["Età"],bins=30,kde=True) # Creo un Istogramma della colonna età con 30 bin, cioè che il range di valori sarà suddiviso in 30 intervalli uguali
-        plt.title("Distribuzione delle Età:") # definisco il titolo
-        plt.xlabel("Età") # Asse X definisco Età
-        plt.ylabel("Frequenza") # Asse Y definisco Frequenza
-        plt.show() # per mostrare il grafico
+        try:
+            plt.figure(figsize=(10,6)) # inizializzo le dimensioni della figura
+            sns.histplot(self.df["Età"],bins=30,kde=True) # Creo un Istogramma della colonna età con 30 bin, cioè che il range di valori sarà suddiviso in 30 intervalli uguali
+            plt.title("Distribuzione delle Età:") # definisco il titolo
+            plt.xlabel("Età") # Asse X definisco Età
+            plt.ylabel("Frequenza") # Asse Y definisco Frequenza
+            plt.show() # per mostrare il grafico
+        except:
+            print("\nErrore, non è stato possibile visualizzare le statistiche relative all'età!")
 
     def visualizza_statistiche_tariffe_mensili(self):
-        plt.figure(figsize=(10,6)) # inizializzo le dimensioni della figura
-        sns.boxplot(x = "Tariffa_Mensile",data=self.df) # Creo un Boxlot 
-        plt.title("Grafico Tariffe Mensili") # definisco il titolo
-        plt.xlabel("Tariffa Mensile") # definisco il nome dell'asse X
-        plt.show() # per mostrare il grafico
+        try:
+            plt.figure(figsize=(10,6)) # inizializzo le dimensioni della figura
+            sns.boxplot(x = "Tariffa_Mensile",data=self.df) # Creo un Boxlot 
+            plt.title("Grafico Tariffe Mensili") # definisco il titolo
+            plt.xlabel("Tariffa Mensile") # definisco il nome dell'asse X
+            plt.show() # per mostrare il grafico
+        except:
+            print("\nErrore, non è stato possibile visualizzare le statistiche relative le tariffe mensili!")
 
 
 
--- mod_Progetto_Python.py
+++ mod_Progetto_Python.py
@@ -7,6 +7,8 @@ def menu(): # funzione menù che stampa le possibili scelte e richiede all'utent
 4: Pulisci i Dati
 5: Prepara i Dati alla modellazione
 6: Esplorazione dei Dati
+7: Visualizza statistiche Età
+8: Visualizza statistiche Tariffe Mensili
 0: Exit
 Indicare la scelta: """
     s = input(info)
@@ -14,24 +16,32 @@ Indicare la scelta: """
 
 def main(): # funzione main che richiama tutte le altre funzioni e il modulo importato
     df = P.DataSet("Corso Python/Giovedì 18/Progetto_Python_1.csv")
-    while True:
-        s = menu()
-        if s == "1":
-            print(f"\nIl Tuo Dataset:\n{df.df}")
-        elif s == "2":
-            df.info()
-        elif s == "3":
-            df.aggiungi_colonna_costo_per_GB()
-        elif s == "4":
-            df.pulizia_dati()
-        elif s == "5":
-            df.preparazione_dati()
-        elif s == "6":
-            df.esplorazione_dati()
-        elif s == "0":
-            print("\nArrivederci!")
-            break
-        else:
-            print("\nErrore!")
+    try:
+        print(f"\nAnteprima Dataset Importato:\n{df.df.head()}\n")
+        while True:
+            s = menu()
+            if s == "1":
+                print(f"\nIl Tuo Dataset:\n{df.df}")
+            elif s == "2":
+                df.info()
+            elif s == "3":
+                df.aggiungi_colonna_costo_per_GB()
+            elif s == "4":
+                df.pulizia_dati()
+            elif s == "5":
+                df.preparazione_dati()
+            elif s == "6":
+                df.esplorazione_dati()
+            elif s == "7":
+                df.visualizza_statistiche_età()
+            elif s == "8":
+                df.visualizza_statistiche_tariffe_mensili()
+            elif s == "0":
+                print("\nArrivederci!")
+                break
+            else:
+                print("\nErrore!\n")
+    except:
+        pass
 
 main()
