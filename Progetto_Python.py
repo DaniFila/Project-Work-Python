1b8c690ea5f9cbdbadfedac306941863c9a17e0a
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-18 15:12:39 +0200

    Add files via upload

--- Progetto_Python.py
+++ Progetto_Python.py
@@ -4,6 +4,7 @@ import numpy as np
 class DataSet:
     def __init__(self,nome_file):
         self.df = pd.read_csv(nome_file)
+        self.nome_file = nome_file
     
     def save_csv(self,nome_file):
         self.df.to_csv(nome_file,index=False)
@@ -15,9 +16,29 @@ class DataSet:
         print(f"Statistiche:\n{self.df.describe()}\n")
         print(f"Stato clienti nella compagnia:\n{self.df['Churn'].value_counts()}\n")
 
-    #def pulizia_dati(self):
+    def pulizia_dati(self):
+        # Sostituzione valori mancanti nelle colonne Tariffa Mensile e Dati Consumate
+        self.df['Tariffa_Mensile'].fillna(self.df['Tariffa_Mensile'].median(), inplace=True)
+        self.df['Dati_Consumati'].fillna(self.df['Dati_Consumati'].mean(), inplace=True)
+
+        # Eliminazione delle righe con valori mancanti rimasti
+        self.df.dropna(inplace=True)
+        print("Eliminate righe con Valori mancanti rimasti\n")
+
+        # Correzione di anomalie
+        self.df = self.df[self.df['Età'] > 0]
+        self.df = self.df[self.df['Tariffa_Mensile'] > 0]
+        self.df = self.df[self.df['Dati_Consumati'] > 0]
+
+        print("DataSet Pulito con successo!\n")
+        self.save_csv(self.nome_file)
 
 
-#a = DataSet("Corso Python/Giovedì 18/Progetto_Python_1.csv")
+
+
+
+
+a = DataSet("Corso Python/Giovedì 18/Progetto_Python_1.csv")
 
 #a.esplorazione_dati()
+
